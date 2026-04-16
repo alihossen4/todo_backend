29cbe1818fe6c9177aca236c6ef987776665726b
alihossen4 <mdahossen.dev@gmail.com>
2026-04-16 23:53:15 +0600

    feat: user model changes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -25,6 +25,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         enum: ['admin', 'member'],
         default: 'member'
+    },
+    addminInviteTokn:{
+        type:Number,
+        default:null
     }
 },
     {timestamps: true}
